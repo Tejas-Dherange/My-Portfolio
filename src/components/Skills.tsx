0d8cb4cf13983625b02e7e36cdc33198a3588a9e
Tejas Dherange <tejasdherange0099@gmail.com>
2024-09-08 02:31:39 +0530

    Add updates in portfolio

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -16,7 +16,7 @@ function Skills() {
       <h1 className="font-bold text-4xl md:mt-10 flex justify-center mt-[-50px]  text-teal-600  tracking-wide uppercase"> My Skills</h1>
       <div className="  flex gap-5 md:gap-10 md:justify-center items-center flex-wrap mt-10 md:mt-16">
         {skils.map((skill) => (
-          <div key={skill.id} className="card p-3 cursor-pointer rounded transform transition-transform duration-300 hover:scale-105 hover:shadow-lg  hover:bg-slate-800  w-[115px] md:w-40 flex flex-col items-center justify-center">
+          <div key={skill.id} className="card p-3 cursor-pointer rounded transform transition-transform duration-300 hover:scale-105 hover:shadow-lg  hover:bg-slate-800  w-[100px] md:w-40 flex flex-col items-center justify-center">
             <Image height={150} width={150} src={`/TeckStack/${skill.image}`} alt={skill.title} />
             <p className="font-bold text-lg">{skill.title}</p>
           </div>
